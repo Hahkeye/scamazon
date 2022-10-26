24d27cc1951f2a428355f8e47ee9a60fb8328192
ivan bonilla <ivanbonilla02@yahoo.com>
2022-10-26 12:53:40 -0500

    product model

---
+++ models/Product.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connect');
+
+class Product extends Model {}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+    },
+    seller_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Seller',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Product',
+  }
+);
+
+module.exports = Product;
