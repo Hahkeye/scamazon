40ceffcb07775c51d9e2995530921419b8d9c118
Alex Oleynik <alexoleynik74@gmail.com>
2022-10-26 14:00:03 -0500

    product seed created

---
+++ seeds/product-seed.js
@@ -0,0 +1,62 @@
+const { Product } = require('../models');
+
+const productData = [
+  {
+    product_name: 'Plain T-Shirt',
+    price: 14.99,
+    stock: 14,
+    category_id: 1,
+  },
+  {
+    product_name: 'Running Sneakers',
+    price: 90.0,
+    stock: 25,
+    category_id: 2,
+  },
+  {
+    product_name: 'Branded Baseball Hat',
+    price: 22.99,
+    stock: 12,
+    category_id: 3,
+  },
+  {
+    product_name: 'Luxury Bottled Air',
+    price: 114.99,
+    stock: 50,
+    category_id: 4,
+  },
+  {
+    product_name: 'Cargo Shorts',
+    price: 29.99,
+    stock: 22,
+    category_id: 5,
+  },
+  {
+    product_name: 'HeadOn',
+    price: 14.99,
+    stock: 14,
+    category_id: 6,
+  },
+  {
+    product_name: 'Power Balance Bracelets',
+    price: 22.66,
+    stock: 14,
+    category_id: 7,
+  },
+  {
+    product_name: 'Shake Weight',
+    price: 14.99,
+    stock: 14,
+    category_id: 8,
+  },
+  {
+    product_name: 'Slap Chop',
+    price: 29.99,
+    stock: 14,
+    category_id: 9,
+  },
+];
+
+const seedProducts = () => Product.bulkCreate(productData);
+
+module.exports = seedProducts;
