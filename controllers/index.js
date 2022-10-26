8ee66400f2f1d7dab5a87ccc8d67cdbedceb9412
Hunter <realisvenator@gmail.com>
2022-10-26 14:13:03 -0500

    file refactor

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const productRoutes = require('./product-routes');
 const cartRoutes = require('./cart-routes');
+const userRoutes = require('./user-route');
 
 router.use('/cart',cartRoutes);
+router.use('/user',userRoutes);
 
 router.use('/', productRoutes);
 module.exports = router;
