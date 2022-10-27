57b630f82841f3590cee5461537e01fa081932df
Tristan Hanson <thanson1998@live.com>
2022-10-27 10:14:30 -0500

    small styling stuff

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const sequelize = require('./config/connect');
 const session = require('express-session');
 const engine = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(routes);
 
