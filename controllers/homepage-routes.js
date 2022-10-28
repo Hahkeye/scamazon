c8cfd7a1b585bb5a1d315591ba6a9e232f4438a1
Tristan Hanson <thanson1998@live.com>
2022-10-28 11:17:27 -0500

    test

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,21 +1,33 @@
 const router = require('express').Router();
 const { User, Product } = require('../models');
 
-router.get('/', async (req, res) =>{
-    try{
-        const dbProduct = await Product.findAll();
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-        const product = dbProduct.map((product) =>
-            product.get({plain: true})
-        );
 
-        res.render('homepage',{
-            product,
-        });
-    } catch(err){
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
+function topProducts(){
+
+for (let i = 0; i < randomNum(1,5); i++) {
+    router.get('/', async (req, res) =>{
+        try{
+            const dbProduct = await Product.findAll();
+
+            const product = dbProduct.map((product) =>
+                product.get({plain: true})
+            );
+
+            res.render('homepage',{
+                product,
+            });
+        } catch(err){
+            console.log(err);
+            res.status(500).json(err);
+        }
+    });
+};
+};
+
+topProducts();
 
 module.exports = router;
